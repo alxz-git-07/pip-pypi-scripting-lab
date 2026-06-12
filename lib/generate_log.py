41084d5a8b5e754920f0d3f6a789a5c084b32967
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-06-12 10:39:52 +0300

    Add automation script with API and file output

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,7 +1,31 @@
 from datetime import datetime
 import os
+import requests
 
 def generate_log(data):
+        if not isinstance(data,list):
+             raise ValueError('This should be a list')
+        
+        filename=f'log_{datetime.now().strftime('%Y%m%d')}.txt'
+
+        with open(filename,'w') as file:
+            for entry in data:
+               file.write(f'{entry}\n')
+
+        print(f'Log written to {filename}')
+
+def fetch_data():
+    response=requests.get('https://jsonplaceholder.typicode.com/posts/1')
+    if response.status_code==200:
+        return response.json()
+    return {}
+
+if __name__== '__main__':
+    post=fetch_data()
+    print('Fetched post Title:',post.get('title','No title found'))
+
+
+
     # TODO: Implement log generation logic
 
     # STEP 1: Validate input
@@ -16,4 +40,4 @@ def generate_log(data):
 
     # STEP 4: Print a confirmation message with the filename
 
-    pass
+    
