13eedf0a38478184ddead2e99bb3e749105c359e
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-06-22 12:00:25 +0300

    modified confitest file

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
 import sys
 import os
 
-# Dynamically add the root project directory to the python path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# Force Python to look inside the CodeGrade student workspace directory
+student_dir = "/home/codegrade/student"
+if student_dir not in sys.path:
+    sys.path.insert(0, student_dir)
+
+# Also check inside the lib directory since generate_log lives there
+lib_dir = os.path.join(student_dir, "lib")
+if lib_dir not in sys.path:
+    sys.path.insert(0, lib_dir)
